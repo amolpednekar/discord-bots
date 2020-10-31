a25ed226d317c6abe9a4d5524f8321f9701e8861
Amol Pednekar <amol_pednekar@persistent.com>
2020-10-31 13:29:15 +0530

    Get historic tweet by ID [WIP]

--- bots/twitter/app.js
+++ bots/twitter/app.js
@@ -7,7 +7,7 @@ const endpointURL = "https://api.twitter.com/2/tweets?ids="
 async function getRequest() {
 
     const params = {
-        "ids": "1278747501642657792,1255542774432063488", // Edit Tweet IDs to look up
+        "ids": "1278747501642657792,1255542774432063488,1321126194633412608", // Edit Tweet IDs to look up
         "tweet.fields": "lang,author_id", // Edit optional query parameters here
         "user.fields": "created_at" // Edit optional query parameters here
     }
